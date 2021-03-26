16421fc91a0215da4411c43750d0fb0f43bf1561
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-03-26 18:56:15 +0530

    pop() method is implemented

---
+++ single-linked-list/SSL-pop.js
@@ -0,0 +1,52 @@
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+
+class SLL {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+	push(val) {
+		let n = new Node(val);
+		if (!this.head) {
+			this.head = n;
+			this.tail = this.head;
+		} else {
+			this.tail.next = n;
+			this.tail = n;
+		}
+		this.length++;
+		return this;
+	}
+	traverse() {
+		let current = this.head;
+		while (current) {
+			console.log(current.val);
+			current = current.next;
+		}
+	}
+	pop() {
+		if (!this.head) return undefined;
+		let current = this.head;
+		let newTail = current;
+		while (current.next) {
+			newTail = current;
+			current = current.next;
+		}
+		this.tail = newTail;
+		this.tail.next = null;
+		return current;
+	}
+}
+
+let list = new SLL();
+list.push('Hello');
+list.push('There');
+list.push('good bye');
+list.pop();
+list.traverse();
