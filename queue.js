f38095b72bb5d25daaa7b93f4aa26e6645ed7dd7
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-03-27 18:48:56 +0530

    deque() method implemented in queue

--- queue.js
+++ queue.js
@@ -23,6 +23,27 @@ class Queue {
 		this.length++;
 		return this.length;
 	}
+	traverse() {
+		let i = 0;
+		let current = this.head;
+		while (current) {
+			console.log(current.val);
+			current = current.next;
+			i++;
+		}
+	}
+	deque() {
+		if (!this.head) return null;
+		let temp = this.head;
+		this.head = this.head.next;
+		temp.next = null;
+		this.length--;
+		if (this.length === 0) {
+			this.head = null;
+			this.tail = null;
+		}
+		return temp.val;
+	}
 }
 
 let queue = new Queue();
